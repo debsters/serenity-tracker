eceed7a3f9399a72f9c4783b48ccee6bb1718abe
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-17 17:31:27 +0000

    more folders

--- lib/serenity_tracker.rb
+++
@@ -1,6 +0,0 @@
-require "serenity_tracker/version"
-
-module SerenityTracker
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/serenity_tracker/version.rb
+++
@@ -1,3 +0,0 @@
-module SerenityTracker
-  VERSION = "0.1.0"
-end
